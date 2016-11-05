1429e7a44fd352d493bb9d800b5fdb706c453383
stm242 <melissa.pangilinan@capitalone.com>
2016-11-04 23:10:13 -0700

    initial commit

---
+++ MovieViewer/DetailViewController.swift
@@ -0,0 +1,58 @@
+//
+//  DetailViewController.swift
+//  MovieViewer
+//
+//  Created by Pangilinan, Melissa on 11/4/16.
+//  Copyright © 2016 Pangilinan, Melissa. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var infoView: UIView!
+    
+    var movie: NSDictionary!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scrollView.contentSize = CGSize(width: scrollView.frame.size.width, height: infoView.frame.origin.y+infoView.frame.size.height)
+        let title = movie["title"] as? String
+        titleLabel.text = title
+        
+        let overview = movie["overview"]
+        overviewLabel.text = overview as? String
+        overviewLabel.sizeToFit()
+        
+        let baseURL = "http://image.tmdb.org/t/p/w500/"
+        if let posterPath = movie["poster_path"] as? String {
+            let posterURL = NSURL(string: baseURL+posterPath)
+            posterImageView.setImageWith(posterURL as! URL)
+        }
+        print(movie)
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- MovieViewer/MoviesViewController.swift
+++ MovieViewer/MoviesViewController.swift
@@ -7,43 +7,87 @@
 //
 
 import UIKit
+import AFNetworking
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var networkErrorView: UIView!
     
     var movies: [NSDictionary]?
+    var refreshControl: UIRefreshControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-
+    
         // Do any additional setup after loading the view.
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(refreshControlAction(refreshControl:)), for: UIControlEvents.valueChanged)
+        tableView.insertSubview(refreshControl, at: 0)
+        
         tableView.dataSource = self
         tableView.delegate = self
         
         let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
-        let url = NSURL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
-        let request = NSURLRequest(url: url! as URL)
-        let session = URLSession(configuration: URLSessionConfiguration.default, delegate:nil, delegateQueue: OperationQueue.main
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
+        let request = URLRequest(url: url! as URL)
+        let session = URLSession(configuration: URLSessionConfiguration.default, delegate: nil, delegateQueue: OperationQueue.main
         )
         
         let task : URLSessionDataTask = session.dataTask(with: request as URLRequest, completionHandler: { (dataOrNil, response, error) in
             if let data = dataOrNil {
+                self.networkErrorView.isHidden = true
+
                 if let responseDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
-                    NSLog("response: \(responseDictionary)")
+                    print("response: \(responseDictionary)")
                     
                     self.movies = responseDictionary["results"] as! [NSDictionary]
                     self.tableView.reloadData()
                 }
+            } else {
+                self.networkErrorView.isHidden = false
             }
         });
         task.resume()
         
+    }
+    
+    func refreshControlAction(refreshControl: UIRefreshControl) {
         
+        // ... Create the NSURLRequest (myRequest) ...
+        let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
+        let request = URLRequest(url: url! as URL)
+        // Configure session so that completion handler is executed on main UI thread
+        let session = URLSession(
+            configuration: URLSessionConfiguration.default,
+            delegate:nil,
+            delegateQueue:OperationQueue.main
+        )
         
-        
-        
+        let task : URLSessionDataTask = session.dataTask(with: request as URLRequest, completionHandler: { (dataOrNil, response, error) in
+            // Tell the refreshControl to stop spinning
+            refreshControl.endRefreshing()
+            
+            // ... Use the new data to update the data source ...
+            if let data = dataOrNil {
+                self.networkErrorView.isHidden = true
+                
+                if let responseDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
+                    print("response: \(responseDictionary)")
+                    
+                    self.movies = responseDictionary["results"] as! [NSDictionary]
+                    
+                    
+                    // Reload the tableView now that there is new data
+                    self.tableView.reloadData()
+                }
+            } else {
+                self.networkErrorView.isHidden = false
+            }
+            
+        });
+        task.resume()
     }
 
     override func didReceiveMemoryWarning() {
@@ -65,19 +109,32 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         let movie = movies![indexPath.row]
         let title = movie["title"] as! String
         let overview = movie["overview"] as! String
+        let baseURL = "http://image.tmdb.org/t/p/w500/"
+        if let posterPath = movie["poster_path"] as? String {
+            let posterURL = NSURL(string: baseURL+posterPath)
+            cell.posterView.setImageWith(posterURL as! URL)
+        }
+        
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
+        
         print("row \(indexPath.row)")
         return cell
     }
-    /*
-    // MARK: - Navigation
+    
+     //MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+     //In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)
+        let movie = movies![indexPath!.row]
+        let detailViewController = segue.destination as! DetailViewController
+        detailViewController.movie = movie
+        
+//         Get the new view controller using segue.destinationViewController.
+//         Pass the selected object to the new view controller.
     }
-    */
+    
 
 }
